99c5eced58e903725f09a14148c4010bfc5e78ee
Francisco Pizarro <francisco_p@outlook.com>
2026-08-04 16:52:43 -0400

    fix(ui): move components.json, add shadcn alias

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -53,6 +53,7 @@ export default defineConfig(({ mode, command }) => {
         { find: "@argos/shared", replacement: path.resolve(projectRoot, "..", "..", "packages", "shared", "src") },
         { find: "#api", replacement: resolve("api") },
         { find: "#shadcn", replacement: resolve("shadcn") },
+        { find: "shadcn", replacement: resolve("shadcn") },
         { find: "#settings", replacement: resolve("settings") },
       ],
     },
